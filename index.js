aa16724a688e4fdaa7c07c32e46bdeb47da001c9
kchapelier <kevin.chapelier@gmail.com>
2015-10-28 12:29:14 +0100

    take advantage of the different indices for vertices, normals and textures in array-to-wavefront-obj 0.1.1

--- index.js
+++ index.js
@@ -3,82 +3,65 @@
 var toObj = require('array-to-wavefront-obj'),
     commentString = '# Converted from a Three.Geometry with three-geometry-to-obj\n';
 
+//FIXME treatVertex and treatNormal are currently the same
+//TODO is it worth it to include a polyfill of es6 Map for vertexMap and normalMap
+
+var treatVertex = function treatVertex (vertex, vertexMap, vertexArray, vertexIndices) {
+    var key = vertex.x + ',' + vertex.y + ',' + vertex.z;
+
+    if (vertexMap.hasOwnProperty(key)) {
+        vertexArray.push(vertex.x);
+        vertexArray.push(vertex.y);
+        vertexArray.push(vertex.z);
+        vertexMap[key] = vertexArray.length / 3;
+    }
+
+    vertexIndices.push(vertexMap[key]);
+};
+
+var treatNormal = function treatNormal (normal, normalMap, normalArray, normalIndices) {
+    var key = normal.x + ',' + normal.y + ',' + normal.z;
+
+    if (normalMap.hasOwnProperty(key)) {
+        normalArray.push(normal.x);
+        normalArray.push(normal.y);
+        normalArray.push(normal.z);
+        normalMap[key] = normalArray.length / 3;
+    }
+
+    normalIndices.push(normalMap[key]);
+};
+
 module.exports = function threeGeometryToObj (geometry, options) {
 
     options = options || {};
 
-    var vertices = new Array(geometry.faces.length * 3),
-        faces = new Array(geometry.faces.length * 3),
-        normals = new Array(geometry.faces.length * 3),
-        face,
-        normal,
-        vertex;
+    var vertexMap = {},
+        normalMap = {},
+        vertices = [],
+        normals = [],
+        vertexIndices = [],
+        normalIndices = [];
+
+    var face;
 
     for (var i = 0; i < geometry.faces.length; i++) {
         face = geometry.faces[i];
 
-        faces[i * 3] = i * 3;
-        faces[i * 3 + 1] = i * 3 + 1;
-        faces[i * 3 + 2] = i * 3 + 2;
-
-        vertex = geometry.vertices[face.a];
-
-        vertices[i * 9] = vertex.x;
-        vertices[i * 9 + 1] = vertex.y;
-        vertices[i * 9 + 2] = vertex.z;
-
-        vertex = geometry.vertices[face.b];
-
-        vertices[i * 9 + 3] = vertex.x;
-        vertices[i * 9 + 4] = vertex.y;
-        vertices[i * 9 + 5] = vertex.z;
-
-        vertex = geometry.vertices[face.c];
-
-        vertices[i * 9 + 6] = vertex.x;
-        vertices[i * 9 + 7] = vertex.y;
-        vertices[i * 9 + 8] = vertex.z;
+        treatVertex(geometry.vertices[face.a], vertexMap, vertices, vertexIndices);
+        treatVertex(geometry.vertices[face.b], vertexMap, vertices, vertexIndices);
+        treatVertex(geometry.vertices[face.c], vertexMap, vertices, vertexIndices);
 
         if (options.normalSource === 'face') {
-            normal = face.normal;
-
-            normals[i * 9] = normal.x;
-            normals[i * 9 + 1] = normal.y;
-            normals[i * 9 + 2] = normal.z;
-
-            normals[i * 9 + 3] = normal.x;
-            normals[i * 9 + 4] = normal.y;
-            normals[i * 9 + 5] = normal.z;
-
-            normals[i * 9 + 6] = normal.x;
-            normals[i * 9 + 7] = normal.y;
-            normals[i * 9 + 8] = normal.z;
+            treatNormal(face.normal, normalMap, normals, normalIndices);
+            treatNormal(face.normal, normalMap, normals, normalIndices);
+            treatNormal(face.normal, normalMap, normals, normalIndices);
         } else {
-            normal = face.vertexNormals[0];
-
-            normals[i * 9] = normal.x;
-            normals[i * 9 + 1] = normal.y;
-            normals[i * 9 + 2] = normal.z;
-
-            normal = face.vertexNormals[1];
-
-            normals[i * 9 + 3] = normal.x;
-            normals[i * 9 + 4] = normal.y;
-            normals[i * 9 + 5] = normal.z;
-
-            normal = face.vertexNormals[2];
-
-            normals[i * 9 + 6] = normal.x;
-            normals[i * 9 + 7] = normal.y;
-            normals[i * 9 + 8] = normal.z;
+            treatNormal(face.vertexNormals[0], normalMap, normals, normalIndices);
+            treatNormal(face.vertexNormals[1], normalMap, normals, normalIndices);
+            treatNormal(face.vertexNormals[2], normalMap, normals, normalIndices);
         }
-
-        /*
-         if (face.faceVertexUvs.length === geometry.faces.length) {
-
-         }
-         */
     }
 
-    return commentString + toObj(vertices, normals, null, faces);
+    return commentString + toObj(vertices, normals, null, vertexIndices, normalIndices);
 };
